8349612bca0c7617940f0ecb437f6bb11fcaf4b1
cold_hailstone <cksn1993@gmail.com>
2023-03-05 14:28:57 +0900

    이것만 알자! 프로그래밍의 3대 요소(변수,자료형,할당)

--- src/TPC01.java
+++ src/TPC01.java
@@ -1,5 +1,7 @@
-public class Main {
+public class TPC01 {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
---
+++ src/TPC02.java
@@ -0,0 +1,24 @@
+public class TPC02 {
+
+    public static void main(String[] args) {
+        // 프로그래밍의 3대 요소: 변수, 자료형(DataType), 할당(=)
+        // 1+1=2
+        int a,b,c;
+        a = 1;
+        b = 1;
+        c = a + b;
+        System.out.println(c);
+
+        float f;
+        f = 34.5F;
+        System.out.println(f);
+
+        char d;
+        d = 'A';
+        System.out.println(d);
+
+        boolean g;
+        g = true; // false(거짓), true(참)
+        System.out.println(g);
+    }
+}
