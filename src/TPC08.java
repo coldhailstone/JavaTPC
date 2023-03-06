6eb7d87d5b70130cc9f4b12543fe032953603361
cold_hailstone <cksn1993@gmail.com>
2023-03-06 22:59:24 +0900

    JVM의 메모리 모델(JVM이 사용하는 메모리 영역 - 4가지)

---
+++ src/TPC08.java
@@ -0,0 +1,11 @@
+public class TPC08 {
+
+    public static void main(String[] args) {
+        int sum = add(30, 50);
+        System.out.println(sum);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
