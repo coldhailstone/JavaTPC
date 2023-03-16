b3f2e4e977e3ff6edd51bf9c95845f3142904a78
cold_hailstone <cksn1993@gmail.com>
2023-03-16 10:57:22 +0900

    private 생성자도 있어요?(static과 관계)

---
+++ src/TPC31.java
@@ -0,0 +1,19 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+
+    public static void main(String[] args) {
+//        Inflearn inflearn = new Inflearn();
+//        inflearn.tpc();
+//        inflearn.java();
+
+        Inflearn.java();
+
+        // Java API 생성자 private
+//        System sys = new System();
+        System.out.println("출력");
+//        Math m = new Math();
+        int v = Math.max(10, 20);
+        System.out.println(v);
+    }
+}
---
+++ src/kr/tpc/Inflearn.java
@@ -0,0 +1,16 @@
+package kr.tpc;
+
+public class Inflearn {
+
+    private Inflearn() {}
+
+    // 인스턴스 메서드
+    public void tpc() {
+        System.out.println("TPC 강의 너무 재밌다.");
+    }
+
+    // 클래스 메서드
+    public static void java() {
+        System.out.println("Java 강의 너무 재밌다.");
+    }
+}
