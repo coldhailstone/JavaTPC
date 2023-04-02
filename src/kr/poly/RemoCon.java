2423bd7fcaff41a2d7ecc9007260f988e3b9786a
cold_hailstone <cksn1993@gmail.com>
2023-04-02 11:59:24 +0900

    인터페이스(실습)

---
+++ src/kr/poly/RemoCon.java
@@ -0,0 +1,11 @@
+package kr.poly;
+
+public interface RemoCon { // 객체생성X
+
+    public static final int MAXCH = 100;
+    public final static int MINCH = 1;
+
+    public void chUp();
+    public void chDown();
+    public void internet();
+}
