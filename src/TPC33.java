6b04810121bb30b6be37e0b3e8bb88572d5e74b4
cold_hailstone <cksn1993@gmail.com>
2023-04-06 13:46:11 +0900

    package(패키지)가 뭐 에요?

---
+++ src/TPC33.java
@@ -0,0 +1,11 @@
+import kr.inflearn.MyClass;
+
+public class TPC33 {
+
+    public static void main(String[] args) {
+//        kr.inflearn.MyClass my = new kr.inflearn.MyClass();
+        MyClass my = new MyClass();
+        int v = my.sum(10, 100);
+        System.out.println(v);
+    }
+}
